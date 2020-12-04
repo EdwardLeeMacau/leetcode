f6881b5edf9c242e3303a4b060340a77140e9a80
Edward.Lee im105 <edward.lee@augentix.com>
2020-12-04 18:23:33 +0800

    Add code for challenge 2020-12-04

---
+++ cpp/the_kth_factor_of_n/solution.cpp
@@ -0,0 +1,6 @@
+#include "solution.h"
+
+int Solution::kthFactor(int n, int k) 
+{
+    return 0;
+}
---
+++ cpp/the_kth_factor_of_n/solution.h
@@ -0,0 +1,8 @@
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+
+class Solution {
+    int kthFactor(int n, int k); 
+};
+
+#endif
