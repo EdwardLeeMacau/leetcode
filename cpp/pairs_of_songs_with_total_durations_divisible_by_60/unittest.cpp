7aa78cf85f357efcda417d96ffee6712024770dc
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2020-12-09 10:17:42 +0800

    Finish code for challenge 2020-12-08

--- cpp/pairs_of_songs_with_total_durations_divisible_by_60/unittest.cpp
+++ cpp/pairs_of_songs_with_total_durations_divisible_by_60/unittest.cpp
@@ -1,6 +1,8 @@
 #include "solution.h"
 #include "gtest/gtest.h"
 
+#include <iostream>
+
 using namespace std;
 
 class SolutionTest : public ::testing::Test {
@@ -9,10 +11,31 @@ class SolutionTest : public ::testing::Test {
 
 TEST_F(SolutionTest, test_0)
 {
-    Solution sol = Solution();
+    Solution sol;
 
     vector<int> test0 = {30, 20, 150, 100, 40};
     EXPECT_EQ(sol.numPairsDivisibleBy60(test0), 3);
+
+    vector<int> test1 = {30, 30, 30};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test1), 3);
+
+    vector<int> test2 = {1, 2, 3};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test2), 0);
+
+    vector<int> test3 = {10, 20, 30, 40, 50, 60};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test3), 2);
+
+    vector<int> test4 = {60, 60, 60};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test4), 3);
+    
+    vector<int> test5 = {60, 60};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test5), 1);
+    
+    vector<int> test6 = {60};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test6), 0);
+    
+    vector<int> test7 = {60, 60, 60, 60};
+    EXPECT_EQ(sol.numPairsDivisibleBy60(test7), 6);
 }
 
 int main(int argc, char** argv)
