6c4b37211daf385eb8db4994f012d0060747bdf5
Edward.Lee im105 <edward.lee@augentix.com>
2020-12-16 16:23:02 +0800

    Add library: utils

---
+++ cpp/utils/bst.h
@@ -0,0 +1,30 @@
+#ifndef BST_H_
+#define BST_H_
+
+#include "binaryTreeNode.h"
+
+#include <stack>
+
+class BST 
+{
+public:
+	BST();
+	BST(TreeNode *);
+	~BST();
+
+	TreeNode *root;
+};
+
+class BSTIterator {
+public:
+	BSTIterator(TreeNode* root);
+
+    int peak();
+	int next();
+	bool hasNext();
+
+private:
+	std::stack<TreeNode*> frontier;
+};
+
+#endif /* !BST_H_ */
