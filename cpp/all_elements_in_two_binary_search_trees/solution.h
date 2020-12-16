85452fabf19de37e6e5007f0e84aa98280abb498
Edward.Lee im105 <edward.lee@augentix.com>
2020-12-16 16:23:52 +0800

    Finish code from Problems set

---
+++ cpp/all_elements_in_two_binary_search_trees/solution.h
@@ -0,0 +1,12 @@
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+
+#include "bst.h"
+
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> getAllElements(TreeNode* root1, TreeNode* root2);
+};
+#endif
