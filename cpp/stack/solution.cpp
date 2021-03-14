a5f6e1745e73dbfad84bde59929d3b1dec54235b
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2021-03-14 15:46:25 +0800

    Add project leetcode/cpp/stack

---
+++ cpp/stack/solution.cpp
@@ -0,0 +1,16 @@
+#include "solution.h"
+
+#include <stack>
+#include <iostream>
+
+/**
+ * @details (Algorithm Descritpion)
+ */
+bool Solution::validateStackSequences(std::vector<int>& pushed, std::vector<int>& popped)
+{
+    int length = pushed.size();
+    int i = 0, j = 0;
+    std::stack<int> buf;
+
+    return false;
+}
---
+++ cpp/stack/solution.h
@@ -0,0 +1,11 @@
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+
+#include <vector>
+
+class Solution {
+public:
+    bool validateStackSequences(std::vector<int>& pushed, std::vector<int>& popped);
+};
+
+#endif /* !SOLUTION_H_ */
