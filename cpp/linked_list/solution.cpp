25ff52a5afe21e634f6f0cb0439cc9edb0971948
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2021-03-14 14:45:10 +0800

    Refactor codes related to Linked-list

--- cpp/linked_list/solution.cpp
+++ cpp/linked_list/solution.cpp
@@ -293,3 +293,68 @@ ListNode* Solution::detectCycle(ListNode *head)
     /** slow_p and fast_p point to the starting point of the loop */
     return fast_p;
 }
+
+/**
+ * @brief utils function for Solution::getIntersectionNode
+ */
+inline bool Solution::isMarked(ListNode *node)
+{
+    return (size_t)node->next & (size_t)MARKER;
+}
+
+/**
+ * @brief utils function for Solution::getIntersectionNode
+ */
+inline void Solution::cleanMarks(ListNode *node)
+{
+    while (node && isMarked(node)) {
+        node->next = (ListNode *)((size_t)node->next & (~((size_t)MARKER)));
+        node = node->next;
+    }
+}
+
+/**
+ * @brief utils function for Solution::getIntersectionNode
+ */
+inline ListNode* Solution::markNode(ListNode *node)
+{
+    ListNode *next = node->next;
+
+    node->next = (ListNode *)((size_t)(node->next) | (size_t)(MARKER));
+
+    return next;
+}
+
+/**
+ * @brief Marked the traversed node by modifing node->next
+ * @score Daily-challenge (runtime / memory) (32.68% / 95.78%)
+ * @score Linked-List     (runtime / memory) (13.15% / 68.08%)
+ * @time-complexity O(N + M). N and M represent the length of listA and listB.
+ * @space-complexity O(1).
+ */
+ListNode* Solution::getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+    ListNode *ans = NULL, *a = headA, *b = headB;
+
+    while (a || b) {
+        if (a && isMarked(a)) {
+            ans = a; break;
+        }
+
+        if (a) {
+            a = markNode(a);
+        }
+
+        if (b && isMarked(b)) {
+            ans = b; break;
+        }
+
+        if (b) {
+            b = markNode(b);
+        }
+    }
+
+    cleanMarks(headA);
+    cleanMarks(headB);
+    return ans;
+}
--- cpp/linked_list/solution.h
+++ cpp/linked_list/solution.h
@@ -12,7 +12,15 @@ struct ListNode
 };
 
 
-class Solution {
+class Solution
+{
+    #define MARKER 0x1
+
+private:
+    inline bool isMarked(ListNode *node);
+    inline void cleanMarks(ListNode *node);
+    inline ListNode* markNode(ListNode *node);
+
 public:
     bool hasCycle(ListNode *head);
     bool isPalindrome(ListNode *head);
@@ -23,6 +31,7 @@ public:
     ListNode* mergeTwoLists(ListNode *l1, ListNode *l2);
     ListNode* removeElements(ListNode *head, int val);
     ListNode* removeNthFromEnd(ListNode *head, int n);
+    ListNode* getIntersectionNode(ListNode *headA, ListNode *headB);
 };
 
 #endif /* !SOLUTION_H_ */
